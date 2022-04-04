948ff81e069a2dc21d6cbce53f443b26a5e47884
SailajaRadjou <p.sailaja@hotmail.com>
2022-04-04 12:49:47 +0200

    made carousel galeries for galerie component

--- src/components/galerie/Galerie.js
+++ src/components/galerie/Galerie.js
@@ -1,14 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import "./Galerie.css";
+import imageHeader1 from "../tinified_images/gal_photo1.jpg";
+import imageHeader2 from "../tinified_images/gal_photo2.jpg";
+import imageHeader3 from "../tinified_images/gal_photo3.jpg";
+import imageHeader4 from "../tinified_images/gal_photo4.jpg";
+import Carousel from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 const Galerie = () => {
+  
   return (
     <Fragment>
         <section id="galerie">
             <div className="container galerie" data-aos="fade-up">
-                <h2>SocialX messenger download</h2>
-                <p className="u-text-small">
-                Our apps are available for download on all stores.
-                </p>
+                <h2>Galeries</h2>
+                <Carousel plugins={['arrows']}>
+                  <img src={imageHeader1} className="galerie_image"/>
+                  <img src={imageHeader2} className="galerie_image"/>
+                  <img src={imageHeader3} className="galerie_image"/>
+                  <img src={imageHeader4} className="galerie_image"/>
+                </Carousel>
             </div>
         </section>  
 
